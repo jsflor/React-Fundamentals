912a47699e08adf346494efc5febaeed83e55fc1
sebastianflor <sebastian-flor@hotmail.com>
2020-02-23 17:19:20 +0100

    Props with spread operator

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from 'react'
 import TarjetaFruta from './components/TarjetaFruta';
 import TarjetaFrutaII from './components/TarjetaFrutaII';
 
-const App = () => (
-    <div>
-        <TarjetaFruta name='Kiwi' price={3} />
-        <TarjetaFrutaII name='Naranja' price={2} />
-    </div>
-)
+class App extends React.Component {
+    state = {
+        name: 'Manzana',
+        price: 4
+    }
+    render() {
+        const otrosDatos = {
+            name: 'Naranja',
+            price: 3
+        };
+        return(
+            <div>
+                <TarjetaFruta name='Kiwi' price={2} />
+                <TarjetaFrutaII name='Pera' price={5} {...otrosDatos}/>
+                <TarjetaFrutaII {...this.state}/>
+            </div>
+        );
+    }
+}
 
 export default App;
