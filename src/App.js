82f96de226954e5f0c3b45fd5e27b58224b3145e
sebastianflor <sebastian-flor@hotmail.com>
2020-03-11 21:21:54 +0100

    ComponentDidMount & ComponentDidUpdate

--- src/App.js
+++ src/App.js
@@ -1,152 +1,116 @@
 import React from 'react'
-import axios from 'axios'
 
-/*http://www.omdbapi.com/*/
+class UserDetails extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {},
+            isFetching: false
+        }
+    }
 
-const Movie = (props) => {
-    const { Title, Plot, Poster } = props.data;
-    return(
-        <div>
-            <h1>{ Title }</h1>
-            <p>
-                { Plot }
-            </p>
-            <img
-                src={ Poster }
-                alt='Poster'
-                style={{
-                width: '150px'
-                }}  
-            /> 
-        </div>
-    );
+    componentDidMount () {
+        this.fetchData()
+    }
+
+    componentDidUpdate (prevProps, prevState) {
+        if (prevProps.userId !== this.props.userId) {
+            this.fetchData();
+        }
+    }
+
+    fetchData = () => {
+        this.setState({
+            isFetching: true
+        });
+
+        const URL = 'https://jsonplaceholder.typicode.com/users/' + this.props.userId;
+        fetch(URL)
+            .then(res => res.json())
+            .then(user => this.setState({
+                user,
+                isFetching: false
+            }));
+    };
+
+    render () {
+        return (
+            <div>
+                <h4>User Details</h4>
+                { this.state.isFetching
+                    ? <h3>Cargando...</h3>
+                    : (
+                        <pre>
+              { JSON.stringify(this.state.user, null, 4 ) }
+            </pre>
+                    )
+                }
+            </div>
+        );
+    }
+}
+
+
+class Events extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: window.innerWidth
+        }
+    }
+
+    componentDidMount () {
+        window.addEventListener('resize', this.handlerResize);
+    }
+
+    handlerResize = () => {
+        this.setState({
+            width: window.innerWidth
+        });
+    };
+
+    render () {
+        return (
+            <div>
+                <h4>Events { this.state.width }</h4>
+            </div>
+        )
+    }
 }
 
 export default class App extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            users: [],
-            isLoading: true,
-            movie: {},
-            isMovieLoading: false,
-            movieLoaded: false,
-            movies: []
-        };
-        this.searchMovie = React.createRef();
-    }
-
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then(users => this.setState(() => ({ users, isLoading: false })))
-        .catch(err => console.error(err));
-    }
-
-    handleChange = (event) => {
-        const query = event.target.value;
-        if(query.length > 2){
-            this.handleQuery(query);
+            id: 1
         }
     }
-
-    handleQuery = async (query) => {
-        const url = 'http://www.omdbapi.com/?apikey=460ba76d';
-        try {
-            const movies = await axios.get(url, {
-                params: {
-                    s: query
-                }
-            });
-
-            this.setState(() => ({ movies: movies.data.Search }));
-        } catch (err) {
-            console.error(err)
-        }
+    componentDidMount () {
+        // Solicitudes HTTP
+        // Agregar Event Listeners
     }
 
-    onSelectMovie = (data) => {
-        const title = data.Title;
-        const input = this.searchMovie.current;
-        input.value = title;
-    }
+    aumentar = () => {
+        this.setState(state => ({
+            id: state.id + 1
+        }));
+    };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        this.setState(() => ({ isMovieLoading: true }));
-        const title = event.target[0].value;
-        const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=460ba76d';
-        /*fetch(url + '&t=' + title)*/
-        axios.get(url, {
-            params: {
-                t: title
-            }
-        })
-        .then(res => this.setState(() => ({ movie: res.data, movieLoaded: true, isMovieLoading: false })))
-        .catch(err => console.error(err));
-        this.setState(() => ({ movies: [], query: '' }));
-    }
-    render(){
-        const { users, isLoading, movie, movieLoaded, isMovieLoading, movies } = this.state;
-        if(isLoading){
-            return <h1>Loading...</h1>
-        }
-        return(
-            <>
-                <h1>HTTP Requests</h1>
-                <ul>
-                    {
-                        users.map((user) => (
-                            <li key={user.id}>
-                                { user.name }
-                                <a href={`http://${user.website}`}>
-                                    website
-                                </a>
-                            </li>
-                        ))
-                    }
-                </ul>
-                <h3>Search movie</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                        type="text"
-                        ref={this.searchMovie}
-                        placeholder="Search by title..."
-                        onChange={this.handleChange}
-                    />
-                    <button>
-                        Search
-                    </button>
-                </form>
-                {
-                    movies && (
-                        <div>
-                            <ul style={{listStyleType: 'none'}}>
-                                {
-                                    movies.map((mov) =>(
-                                        <li
-                                            key={mov.imdbID}
-                                            onClick={this.onSelectMovie.bind(this, mov)}
-                                        >
-                                            {mov.Title}
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                        </div>
-                    )
-                }
-                {
-                    isMovieLoading && (
-                        <h3>Loading movie...</h3>
-                    )
-                }
-                {
-                    movieLoaded && !isMovieLoading
-                    ? <Movie data={movie} />
-                    : null
-                }
-            </>
-        );
+    render () {
+        const { id } = this.state;
+        return (
+            <div>
+                <h1>componentDidMount</h1>
+                <Events />
+                <h1>componentDidUpdate</h1>
+                <h4>ID: { id }</h4>
+                <button onClick={this.aumentar}>
+                    Aumentar
+                </button>
+                <UserDetails
+                    userId={id}
+                />
+            </div>
+        )
     }
 }
